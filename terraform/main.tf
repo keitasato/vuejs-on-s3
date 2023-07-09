d77b4afeb563115c8c5b66edde48b6b3bff5ef2a
sato-k <sato-k@i-sol.jp>
2023-07-09 22:15:50 +0900

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+# Config
+terraform {
+  required_version = ">=0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+}
+
+# Provider
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
+
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,2 @@
+project     = "vuejs-on-s3"
+environment = "prod"
